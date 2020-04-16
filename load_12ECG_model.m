dbde5823d5bd47d1deb221ef4dd9bb050c2100fd
giovanni-bort <63361927+giovanni-bort@users.noreply.github.com>
2020-04-16 18:18:52 +0200

    Add files via upload

--- load_12ECG_model.m
+++ load_12ECG_model.m
@@ -1,8 +1,9 @@
 function model = load_12ECG_model()
-
-        filename='finalized_model.mat';
-        A=load(filename);
-        model=A.model;
+     % CINC20_ivaylo_P2
+    
+         filename='trainedGN_04_06.mat';
+         A=load(filename);
+         model=A.trainedGN;
 
 end
 
